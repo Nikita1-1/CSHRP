3450305d9f40edf8b463118d2bf5d091c3caee99
nikita <sazonovn2018@gmail.com>
2023-11-29 13:19:47 -0700

    new project of bloggie

---
+++ Bloggie.Web/Bloggie/Data/BlogDBContext.cs
@@ -0,0 +1,19 @@
+﻿using Bloggie.Models.Domain;
+using Bloggie.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace Blog.Web.Data
+
+{
+    public class BlogDBContext : DbContext
+    {
+        public BlogDBContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<BlogPost> BlogPosts { get; set; }
+
+        public DbSet<Tag> Tags { get; set; }
+    }
+}
