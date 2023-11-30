3487a1ec382f8f4af58907b2c86026e6f4bd5053
nikita <sazonovn2018@gmail.com>
2023-11-30 12:54:55 -0700

    added funcrionality to form and created Tags page

---
+++ Bloggie.Web/Bloggie/Controllers/AdminTagsController.cs
@@ -0,0 +1,25 @@
+﻿using Bloggie.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bloggie.Controllers
+{
+    public class AdminTagsController : Controller
+    {
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ActionName("Add")]
+        //collectiong data from form
+        public IActionResult Add(AddTagRequest addTagRequest)
+        {
+            var name = addTagRequest.Name;
+            var displayName = addTagRequest.DisplayName;
+
+            return View("Add");
+        }
+    }
+}
