8e639f5d92f8ccac0eb7ea752b5b875c0cc69891
nikita <sazonovn2018@gmail.com>
2023-12-12 17:25:29 -0700

    delete functionality and async funct

--- Bloggie.Web/Bloggie/Controllers/AdminTagsController.cs
+++ Bloggie.Web/Bloggie/Controllers/AdminTagsController.cs
@@ -2,8 +2,10 @@
 using Bloggie.Models.Domain;
 using Bloggie.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Identity.Client;
 using Microsoft.IdentityModel.Tokens;
+using System.Reflection.Metadata.Ecma335;
 
 namespace Bloggie.Controllers
 {
@@ -41,12 +43,65 @@ namespace Bloggie.Controllers
 
         [HttpGet]
         [ActionName("List")]
-        public IActionResult List()
+        public async Task<IActionResult> List()
         {
             //use dbContext to read the tags
-            var tags = blogDBContext.Tags.ToList();
-
+            var tags = await blogDBContext.Tags.ToListAsync();
+            
             return View(tags);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(Guid id)
+        {
+            var tag = await blogDBContext.Tags.FirstOrDefaultAsync(x => x.Id == id);
+            
+
+            if (tag != null) 
+            {
+                var editTagRequest = new EditTagRequest
+                {
+                    Id = tag.Id,
+                    Name = tag.Name,
+                    DisplayName = tag.DisplayName
+                };
+                return View(editTagRequest);
+            }
+            return View(null);
+        }
+
+        [HttpPost]
+        
+        public async Task<IActionResult> Edit(EditTagRequest editTagRequest)
+        {
+            var tag = new Tag { 
+                Id = editTagRequest.Id,
+                Name = editTagRequest.Name,
+                DisplayName = editTagRequest.DisplayName
+            };
+            var existingTag = await blogDBContext.Tags.FindAsync(tag.Id);
+            if(existingTag != null)
+            {
+                existingTag.Name = tag.Name;
+                existingTag.DisplayName = tag.DisplayName; 
+
+                await blogDBContext.SaveChangesAsync();
+                return RedirectToAction("List", new { id = editTagRequest.Id });
+            }
+            return RedirectToAction("Edit", new { id = editTagRequest.Id });
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete (EditTagRequest editTagRequest)
+        {
+            var tag = await blogDBContext.Tags.FindAsync(editTagRequest.Id);
+             if(tag != null)
+            {
+                blogDBContext.Tags.Remove(tag);
+                await blogDBContext.SaveChangesAsync();
+            return RedirectToAction("List");
+            }
+        return RedirectToAction("Edit", new {id=editTagRequest.Id});
         }
     }
 }
